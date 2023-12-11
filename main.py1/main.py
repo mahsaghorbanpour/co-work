c511d236c7c51c4d89d31067206a3a08b25be82d
mahsaghorbanpour <mahsaghorbanpour1382@gmail.com>
2023-12-11 22:51:25 +0330

    changed

--- main.py1/main.py
+++ main.py1/main.py
@@ -1,13 +1,14 @@
-import random
-suits = ["del" , "khesht" , "geshniz" , "pick"]
-faces = ["shah" , "bi bi" , "tak" , "sarbaz"]
-nums = [2 , 3 , 4 , 5 , 6 , 7 , 8 , 9 , 10]
+def generate game set(suits,faces+nums):
+    card = random.choice(list(deck))
+    deck.remove(card)
+    suits = ["del" , "khesht" , "geshniz" , "pick"]
+    faces = ["shah" , "bi bi" , "tak" , "sarbaz"]
+    nums = [2 , 3 , 4 , 5 , 6 , 7 , 8 , 9 , 10]
 deck = set()
 for suit in suits:
     for card in faces + nums:
         deck.add((card , "of" , suit ))
-def draw():
-    card = random.choice(list(deck))
-    deck.remove(card)
-    return card
-print (deck)
+return deck
+gameset = generate game set (suits,face+nums)
+print gameset
+
