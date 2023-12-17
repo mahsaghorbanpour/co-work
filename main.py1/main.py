0ca3e887bb6500ec65f820cc21d94cc6a9170e40
mahsaghorbanpour <mahsaghorbanpour1382@gmail.com>
2023-12-17 14:09:54 +0330

    change to 4barg

--- main.py1/main.py
+++ main.py1/main.py
@@ -10,15 +10,28 @@ faces = ["shah" , "bi bi" , "tak" , "sarbaz"]
 nums = [2 , 3 , 4 , 5 , 6 , 7 , 8 , 9 , 10]
 card = faces + nums
 gameset = generate_game_set (suits,card)
+earth = []
 amin = []
 mahsa = []
-for i in range(5):
+for i in range(4):
     card = random.choice(list(gameset))
-    amin.append(card)
+    earth.append(card)
     gameset.remove(card)
-for _ in range(5):
-    card = random.choice(list(gameset))
-    mahsa.append(card)
-    gameset.remove(card)
+while len(gameset) != 0 :
+    for i in range(4):
+        card = random.choice(list(gameset))
+        amin.append(card)
+        gameset.remove(card)
+    for _ in range(4):
+        card = random.choice(list(gameset))
+        mahsa.append(card)
+        gameset.remove(card)
+print(earth)
+print(amin)
+print(mahsa)
+
+
+
+
 
 
