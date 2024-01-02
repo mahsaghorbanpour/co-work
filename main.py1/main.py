a22ec264d2db2611e3c6c29beacd176ca5364253
Amin <39793401+AminGhorbani96@users.noreply.github.com>
2024-01-02 01:53:28 -0800

    Update main.py
    add soldier

--- main.py1/main.py
+++ main.py1/main.py
@@ -1,6 +1,14 @@
 import random
 
 
+def calculate_point(list_of_point):
+    point = 0
+    for p in list_of_point:
+        if p[2] == "geshniz":
+            point+=1
+    return point
+
+
 def total_11(num1, num2):
     if (type(num1) is int) and (type(num2) is int):
         result = num1 + num2
@@ -17,6 +25,7 @@ def total_11(num1, num2):
     else:
         return False
 
+
 def generate_game_set(suits,cards):
     deck = set()
     for suit in suits:
@@ -25,13 +34,13 @@ def generate_game_set(suits,cards):
     return deck
 
 
+
 suits = ["del", "khesht", "geshniz", "pick"]
-faces = ["shah", "bi bi", "tak", "sarbaz"]
-nums = [2, 3, 4, 5, 6, 7, 8, 9, 10]
+faces = ["shah", "bi bi", "sarbaz"]
+nums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 card = faces + nums
 
 gameset = generate_game_set(suits, card)
-print(gameset)
 
 earth = []
 amin = []
@@ -59,79 +68,78 @@ while len(gameset) != 0:
         user_input =int(input("please select the card index :"))
         card = amin[user_input]
         amin.remove(card)
-        is_win = False
-        for j in earth:
-            print("card of amin : ", card)
-            print("card of j : ", j)
-            if total_11(j[0], card[0]) == True:
-                win_amin.append(j)
+        sarbaz_winner= []
+        if card[0] == "sarbaz":
+            number_existance = False
+            for m in range(len(earth)):
+                earth_card = earth[m]
+                if (type(earth_card[0]) is int) or (earth_card[0] == "sarbaz"):
+                    win_amin.append(earth_card)
+                    sarbaz_winner.append(earth_card)
+                    number_existance = True
+                else:
+                    continue
+            if number_existance == True:
                 win_amin.append(card)
-                is_win = True
-                break
-        if is_win == False:
-            earth.append(card)
+                for s in sarbaz_winner:
+                    earth.remove(s)
+                sarbaz_winner = []
+            else:
+                earth.append(card)
+        else:
+            is_win = False
+            for j in earth:
+                if total_11(j[0], card[0]) == True:
+                    win_amin.append(j)
+                    earth.remove(j)
+                    win_amin.append(card)
+                    is_win = True
+                    break
+            if is_win == False:
+                earth.append(card)
 
         print("==================================")
         print("cards of earth = ", earth)
         print("cards of mahsa = ", mahsa)
-        user_input =int(input("please select the card index :"))
+        user_input = int(input("please select the card index :"))
         card = mahsa[user_input]
         mahsa.remove(card)
-        mahsa_flag = False
-        for a in earth:
-            if total_11(a[0], card[0]) == True:
-                win_mahsa.append(a)
+        msarbaz = False
+        if card[0] == "sarbaz":
+            msarbaz_winner = []
+            for l in range(len(earth)):
+                m_earth_card = earth[l]
+                if type(m_earth_card[0]) is int or m_earth_card[0] == "sarbaz":
+                    win_mahsa.append(m_earth_card)
+                    msarbaz_winner.append(m_earth_card)
+                    msarbaz = True
+            if msarbaz == True:
                 win_mahsa.append(card)
-                mahsa_flag = True
-                break
-        if mahsa_flag == False:
-            earth.append(card)
+                for s in msarbaz_winner:
+                    earth.remove(s)
+                msarbaz_winner = []
+
+            else:
+                earth.append(card)
+        else:
+            mahsa_flag = False
+            for a in earth:
+                if total_11(a[0], card[0]) == True:
+                    win_mahsa.append(a)
+                    win_mahsa.append(card)
+                    earth.remove(a)
+                    mahsa_flag = True
+                    break
+            if mahsa_flag == False:
+                earth.append(card)
         print("one set finished")
         print("wins of amin", win_amin)
         print("wins of mahsa", win_mahsa)
 
-#
-# def mahsa(user_color, user_age):
-#     message = "{} is very beauty".format(user_color)
-#     print(message)
-#     new_age = user_age + 1
-#     return new_age
-#
-#
-# def amin(job, experience):
-#     new_ex = experience + 1
-#     message = "amin is a {}".format(job)
-#     print(message)
-#     return new_ex
-#
-# message ="This is our scope sample"
-#
-# print("welcome {}".format(message))
-#
-# amin_job = "programmer"
-# amin_exp = 2
-#
-# mahsa_color = "dark red"
-# mahsa_age = 20
-#
-# amin_result = amin(job=amin_job, experience= amin_exp)
-# mahsa_result=mahsa(user_color=mahsa_color, user_age= mahsa_age)
-#
-#
-# print(message)
-# print(amin_result)
-# print(mahsa_result)
-#
-#
-#
-#
-#
-#
-#
+point_of_amin = calculate_point(win_amin)
+point_of_mahsa = calculate_point(win_mahsa)
 
-
-
-
-
-
-
+if point_of_amin > point_of_mahsa:
+    print("Amin is win game with grade :", point_of_amin)
+else:
+    print("Mahsa is win game with grade :", point_of_mahsa)
